5d1a19955a692cff86a4853bb11675f5cb245719
Sanyuja Zagade <sanyujaz6205@gmail.com>
2026-07-02 19:08:34 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -53,7 +53,12 @@ app.use("/api/messages", messageRouter);
 await connectDB();
 
 // Run server on PORT
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, ()=>{
-  console.log("Server is running on PORT: " + PORT);
-});
+if(process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, ()=>{
+    console.log("Server is running on PORT: " + PORT);
+  });
+}
+
+// Export server for Versel
+export default server;
